5b0e4c3ccef5aa82b21e299e2eeb0dc0e565f0a7
MacMini_Techmodi <Techteam@MACMINI.local>
2017-08-09 18:55:32 +0530

    local References

--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -5,11 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './binding.component.html',
   styleUrls: ['./binding.component.css']
 })
+
 export class BindingComponent implements OnInit {
 dt:String
 mytype='text';
 defineType="text"
 brd:String;
+
+
   constructor() {   }
 
   ngOnInit() {
