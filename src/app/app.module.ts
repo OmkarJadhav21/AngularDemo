13d7f4f0624ec8093a85caf73f2e36dc109e7755
MacMini_Techmodi <Techteam@MACMINI.local>
2017-08-14 13:35:56 +0530

    ng2-Table Import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { FormsModule } from '@angular/forms';
 import { BindingComponent } from './binding/binding.component';
 import { InterCompCommuComponent } from './binding/inter-comp-commu.component';
 import { HostbindComponent } from './hostbind/hostbind.component';
@@ -12,9 +10,11 @@ import { HostDirDirective } from './hostbind/host-dir.directive';
 import { StructDirctvComponent } from './struct-dirctv/struct-dirctv.component';
 import { CustmStrutDirctvComponent } from './custm-strut-dirctv/custm-strut-dirctv.component';
 import { CustmDirctvDirective } from './custm-strut-dirctv/custm-dirctv.directive';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
+import { TableComponent } from './table/table.component';
+import { FormsModule } from '@angular/forms';
 
-
-@NgModule({
+@NgModule({ 
   declarations: [
     AppComponent,
     HeaderComponent,
@@ -26,12 +26,15 @@ import { CustmDirctvDirective } from './custm-strut-dirctv/custm-dirctv.directiv
     StructDirctvComponent,
     CustmStrutDirctvComponent,
     CustmDirctvDirective,
+    TableComponent,
+   
   
    
   ],
   imports: [
     BrowserModule,
-     FormsModule
+     FormsModule,
+     Ng2TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
