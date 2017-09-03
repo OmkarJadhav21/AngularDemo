0b225410f0f23e51ec14bdca34880f7e03bea809
omkarjadhav21 <omkarjadhav7110@gmail.com>
2017-09-03 14:20:03 +0530

    Service Emit and Subscribe

--- src/app/service/insta.component.ts
+++ src/app/service/insta.component.ts
@@ -1,3 +1,4 @@
+import { WebserviceService } from './webservice.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,17 +6,21 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <div class="row well">
   <div class="col-md-12">
-      Value of date in Insta is {{token}}
+      Value of date in Insta is {{iToken}}
   </div>
 </div>
   `,
   styles: []
 })
 export class InstaComponent implements OnInit {
+  
+iToken:String;
 
-  constructor() { }
+  constructor(private web:WebserviceService) { }
 
   ngOnInit() {
+    this.iToken=this.web.token
+    this.web.subscribeToken(iToken=>this.iToken=iToken)
   }
 
 }
--- src/app/service/webservice.service.ts
+++ src/app/service/webservice.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable()
 export class WebserviceService {
 token:String;
-  constructor() { }
-  
+em:EventEmitter<String>
+
+  constructor() { 
+    this.em=new EventEmitter<String>();
+  }
+
 getToken(){
   this.token=" "+new Date().getMilliseconds();
+  this.em.emit(this.token);
   return this.token
+}
+subscribeToken(myVar:any){
+  this.em.subscribe(StrTkn=>{
+    myVar(StrTkn)
+  })
+
 }
 }
